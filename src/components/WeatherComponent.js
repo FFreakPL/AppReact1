6805faaa544dcab7fd48dcdf70c82f427810f48c
Paweł Kubiak <ffreakpl@MacBook-Air-Pawe.local>
2022-06-11 10:50:53 +0200

    drobne stylowanie

--- src/components/WeatherComponent.js
+++ src/components/WeatherComponent.js
@@ -8,11 +8,14 @@ function WeatherComponent({weatherData, props,}) {
                 <div className="weather">
                     <h1>{props.name}</h1>
                     <h2>Prognoza na 5 dni</h2>
-                    <p>{weatherData.list[3].dt_txt}: <strong>{weatherData.list[3].main.temp}</strong></p>
-                    <p>{weatherData.list[11].dt_txt}: <strong>{weatherData.list[11].main.temp}</strong></p>
-                    <p>{weatherData.list[19].dt_txt}: <strong>{weatherData.list[19].main.temp}</strong></p>
-                    <p>{weatherData.list[27].dt_txt}: <strong>{weatherData.list[27].main.temp}</strong></p>
-                    <p>{weatherData.list[35].dt_txt}: <strong>{weatherData.list[35].main.temp}</strong></p>
+                    <div className="weather_container">
+                        <p><strong>{weatherData.list[3].main.temp}</strong></p>
+                        <p><strong>{weatherData.list[11].main.temp}</strong></p>
+                        <p><strong>{weatherData.list[19].main.temp}</strong></p>
+                        <p><strong>{weatherData.list[27].main.temp}</strong></p>
+                        <p><strong>{weatherData.list[35].main.temp}</strong></p>
+                    </div>
+
                 </div>
         </>
     )
